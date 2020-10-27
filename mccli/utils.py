424d69050668bf06fae271990cb5b5019cef5217
Alve Svarén <alve@hotmail.se>
2020-10-28 00:57:27 +0100

    Added choice dialog

--- mccli/utils.py
+++ mccli/utils.py
@@ -1,3 +1,4 @@
+from typing import Dict, List, Union
 
 
 def confirm(msg: str, default: bool = False) -> bool:
@@ -19,3 +20,31 @@ def confirm(msg: str, default: bool = False) -> bool:
         print()
         return False
     return value in ["yes", "y"] or (default and not value)
+
+
+def choice(msg: str, alternatives: List[str], default: int = 0) -> int:
+    """
+    Display multiple choice dialog to select between a list of alternatives
+
+    ```py
+    if choice("What is your favorite color?", ["Red", "Green", "Blue"], 1) == 2:
+        print("Ok, blue is your favorite color.")
+    ```
+    """
+
+    print(msg)
+
+    for i, alternative in enumerate(alternatives):
+        print(f"  {alternative}: [{i}]")
+    try:
+        value = input(f"Enter number: [{default}] ")
+        if not value:
+            value = default
+        else:
+            value = int(value)
+    except (KeyboardInterrupt, ValueError):
+        print()
+        return -1
+    if value >= 0 and value < len(alternatives):
+        return value
+    return -1
