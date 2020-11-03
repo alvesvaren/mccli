dc152646b02cd2f76ddc94ccfd8c521b50be707a
Alve Svarén <alve@hotmail.se>
2020-11-04 00:16:27 +0100

    More things to systemd thing

--- mccli/systemd.py
+++ mccli/systemd.py
@@ -1,6 +1,7 @@
 
 from enum import Enum
 from typing import Union
+import dbus
 
 
 class SystemdActiveState(Enum):
@@ -20,6 +21,22 @@ class SystemdLoadState(Enum):
     MASKED = "masked"
 
 
+class SystemdEnablementState(Enum):
+    ENABLED = "enabled"
+    ENABLED_RUNTIME = "enabled-runtime"
+    LINKED = "linked"
+    LINKED_RUNTIME = "linked-runtime"
+    ALIAS = "alias"
+    MASKED = "masked"
+    MASKED_RUNTIME = "masked-runtime"
+    STATIC = "static"
+    INDIRECT = "indirect"
+    DISABLED = "disabled"
+    GENERATED = "generated"
+    TRANSIENT = "transient"
+    BAD = "bad"
+
+
 class SystemdStatusState(Enum):
     OK = 0
     DEAD_PID = 1
@@ -28,9 +45,32 @@ class SystemdStatusState(Enum):
     UNKNOWN = 4
 
 
+class BusType(Enum):
+    SYSTEM = 0
+    SESSION = 1
+
+
 class Unit:
-    def __init__(self, name: str):
+    def __init__(self, name: str, bus: BusType):
+        if not name.endswith((
+            ".service", ".target",
+            ".socket", ".device", ".mount",
+            ".automount", ".swap", ".path",
+            ".timer", ".snapshot", ".scope"
+        )):
+            name += ".service"
         self.name = name
+        self._bus = dbus.SystemBus() \
+            if bus == BusType.SYSTEM \
+            else dbus.SessionBus()
+        self._systemd = self._bus.get_object(
+            'org.freedesktop.systemd1',
+            '/org/freedesktop/systemd1'
+        )
+        self._manager = dbus.Interface(
+            self._systemd,
+            'org.freedesktop.systemd1.Manager'
+        )
 
     def start(self) -> int:
         """
@@ -53,8 +93,18 @@ class Unit:
     def disable(self, now: bool = False):
         raise NotImplementedError
 
-    def status(self):
+    def reload(self):
         raise NotImplementedError
+
+    @property
+    def status(self) -> SystemdStatusState:
+        raise NotImplementedError
+
+    @property
+    def enabled(self) -> SystemdEnablementState:
+        return SystemdEnablementState(
+            self._manager.GetUnitFileState(self.name)
+        )
 
 
 class Service(Unit):
