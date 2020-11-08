c1743528f55b36fb94fb525ba434dad543fd1d9d
Alve Svarén <alve@hotmail.se>
2020-11-08 22:56:03 +0100

    Added upgrade command (only works if the repo is owned by minecraft user)

--- mccli.sh
+++ mccli.sh
@@ -2,5 +2,4 @@
 # This is just a simple file to allow 
 # mccli to be used directly without 
 # running it by invoking it with python
-
 sudo -u minecraft python3 -m mccli "$@"
--- mccli/cli_parser.py
+++ mccli/cli_parser.py
@@ -1,5 +1,8 @@
 import argparse
 from argparse import Namespace
+import os
+import mccli
+from pathlib import Path
 from mccli.server_utils import Server
 from mccli.online_utils import ServerProvider
 from .commands import (create, modify, update, run)
@@ -16,16 +19,20 @@ commands = {
     "run": subparsers.add_parser("run")
 }
 
-commands_not_requires_server = {
+commands_optional_server = {
     "create": subparsers.add_parser("create")
+}
+
+commands_no_server = {
+    "upgrade": subparsers.add_parser("upgrade")
 }
 
 for k, v in commands.items():
     v.add_argument("server")
-
-for k, v in commands_not_requires_server.items():
+commands.update(commands_optional_server)
+for k, v in commands_optional_server.items():
     v.add_argument("server", default="", nargs="?")
-commands.update(commands_not_requires_server)
+commands.update(commands_no_server)
 
 # Create command
 commands["create"].add_argument(
@@ -51,11 +58,16 @@ def status_wrapper(args: Namespace):
 def run_wrapper(args: Namespace):
     run(name=args.server, verbose=args.verbose)
 
+def upgrade(args: Namespace):
+    os.chdir(Path(mccli.__file__).parent.resolve())
+    exit(os.system("git pull --ff-only"))
+
 commands["create"].set_defaults(runner=create_wrapper)
 commands["modify"].set_defaults(runner=modify_wrapper)
 commands["update"].set_defaults(runner=update_wrapper)
 commands["status"].set_defaults(runner=status_wrapper)
 commands["run"].set_defaults(runner=run_wrapper)
+commands["upgrade"].set_defaults(runner=upgrade)
 parser.set_defaults(runner=status_wrapper)
 
 
