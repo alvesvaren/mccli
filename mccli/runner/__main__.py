d843965959bccdea2c68f019b4027aa5358cd358
Alve Svarén <alve@hotmail.se>
2020-11-08 19:48:52 +0100

    Fixed main for script

--- mccli/runner/__main__.py
+++ mccli/runner/__main__.py
@@ -88,9 +88,7 @@ if __name__ == "__main__":
     server = mccli.Server(sys.argv[1])
     os.chdir(server.path)
     if len(sys.argv) > 2:
-        if sys.argv[2] == "system":
-            exit(system_open())
-    subprocess_open()
-    
-        
-
+        if sys.argv[2] == "subproc":
+            subprocess_open()
+            exit()
+    exit(system_open())
