eb355d15db749023ddb31b2b5d9260ba3741727d
Alve Svarén <alve@hotmail.se>
2020-11-08 19:50:19 +0100

    Removed unused code in runner and formatting

--- mccli/runner/__main__.py
+++ mccli/runner/__main__.py
@@ -5,67 +5,21 @@ import subprocess
 import sys
 import mccli
 import os
-import time
 from threading import Thread
-import tty
-import termios
-import select
-
-
-# def enqueue_output(out, queue):
-#     for line in iter(out.readline, b''):
-#         queue.put(line)
-#     out.close()
-
-class LocalShell():
-    def __init__(self):
-        pass
-
-    def run(self):
-        env = os.environ.copy()
-        p = Popen(["java", "-jar", "server.jar", "nogui"], stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.STDOUT, shell=True, env=env)
-        sys.stdout.write("Started Local Terminal...\r\n\r\n")
-
-        def writeall(p):
-            while True:
-                # print("read data: ")
-                data = p.stdout.read(1).decode("utf-8")
-                if not data:
-                    break
-                sys.stdout.write(data)
-                sys.stdout.flush()
-
-        writer = Thread(target=writeall, args=(p,))
-        writer.start()
-
-        try:
-            while True:
-                d = sys.stdin.read(1)
-                if not d:
-                    break
-                self._write(p, d.encode())
-
-        except EOFError:
-            pass
-
-    def _write(self, process, message):
-        process.stdin.write(message)
-        process.stdin.flush()
-
-
-
 
 
 def handler(process: Popen):
     while process.poll() is None:
-        
         cmd = input("> ").encode("utf-8")
         print(cmd)
         process.stdin.write(cmd + b"\n")
         process.stdin.flush()
 
+
 def subprocess_open():
-    process = subprocess.Popen("java -jar server.jar nogui", stdin=subprocess.PIPE, shell=True, stdout=subprocess.PIPE, env=os.environ.copy())
+    process = subprocess.Popen(
+        "java -jar server.jar nogui", stdin=subprocess.PIPE,
+        shell=True, stdout=subprocess.PIPE, env=os.environ.copy())
     # process.communicate()
     thread = Thread(target=handler, args=[process], daemon=True)
     thread.start()
@@ -78,11 +32,10 @@ def subprocess_open():
     except KeyboardInterrupt:
         process.kill()
 
-    # shell = LocalShell()
-    # shell.run()
 
 def system_open():
     return os.system("java -jar server.jar nogui")
+
 
 if __name__ == "__main__":
     server = mccli.Server(sys.argv[1])
