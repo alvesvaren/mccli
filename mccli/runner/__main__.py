1e58371f253caa2736584861248511fcbe637a9c
Alve Svarén <alve@hotmail.se>
2020-11-08 19:52:01 +0100

    Moved some code around to better fit pythonistic file structure

--- mccli/runner/__main__.py
+++ mccli/runner/__main__.py
@@ -1,40 +1,7 @@
-import queue
-from subprocess import Popen
 import mccli
-import subprocess
 import sys
-import mccli
 import os
-from threading import Thread
-
-
-def handler(process: Popen):
-    while process.poll() is None:
-        cmd = input("> ").encode("utf-8")
-        print(cmd)
-        process.stdin.write(cmd + b"\n")
-        process.stdin.flush()
-
-
-def subprocess_open():
-    process = subprocess.Popen(
-        "java -jar server.jar nogui", stdin=subprocess.PIPE,
-        shell=True, stdout=subprocess.PIPE, env=os.environ.copy())
-    # process.communicate()
-    thread = Thread(target=handler, args=[process], daemon=True)
-    thread.start()
-    try:
-        while process.poll() is None:
-            # process.communicate(timeout=0.5)
-            line = process.stdout.readline()
-            if line:
-                print(line.decode("utf-8"), end="")
-    except KeyboardInterrupt:
-        process.kill()
-
-
-def system_open():
-    return os.system("java -jar server.jar nogui")
+from . import subprocess_open, system_open
 
 
 if __name__ == "__main__":
