b254db737ac49c235fa7ffbde7fa2e76958dadb2
Alve Svarén <alve@hotmail.se>
2020-11-15 17:33:40 +0100

    Bumped version

--- mccli/options/__init__.py
+++ mccli/options/__init__.py
@@ -10,7 +10,7 @@ OPTIONS = {
         "server_jar": "server.jar",
         "xdg_runtime_dir": "/run/user/{}"
     },
-    "version": "0.1.4",
+    "version": "0.1.5",
     "service_template_name": "minecraft-server@{}.service",
     "verbose_output": False
 }
