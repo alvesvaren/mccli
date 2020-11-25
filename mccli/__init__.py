2ab846c2c20055082ea5987e27cbce01d56dfd6f
Alve Svarén <alve@hotmail.se>
2020-11-25 17:51:51 +0100

    Added waterfall to __init__ exports

--- mccli/__init__.py
+++ mccli/__init__.py
@@ -1,6 +1,6 @@
 from .online_utils import (
-    get_paper_versions, get_vanilla_versions,
-    get_versions, PaperVersion, ServerProvider,
+    get_paper_versions, get_vanilla_versions, get_waterfall_versions,
+    get_versions, PaperVersion, WaterfallVersion, ServerProvider,
     ServerVersion, VanillaVersion, VanillaVersionType, find_version, get_version
 )
 
